457aa87b9807178474ce4a08b8435e4a0c021a48
YouenEtrillard <contact@youen-etrillard.com>
2022-04-20 20:40:50 +0200

    make loginUser functional

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,7 +33,7 @@ const registerUser = asyncHandler(async (req, res) => {
     password: hashedPassword,
   });
 
-  if(user) {
+  if (user) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
@@ -49,7 +49,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login User"})
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid credentials');
+  }
 });
 
 // @desc    Get user data 
