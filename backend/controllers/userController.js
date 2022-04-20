386df98b922698456646e70ba4f90a78189e35aa
YouenEtrillard <contact@youen-etrillard.com>
2022-04-20 20:04:02 +0200

    add user related endpoints

---
+++ backend/controllers/userController.js
@@ -0,0 +1,26 @@
+// @desc Register 
+// @route POST /api/users
+// @access Public
+const registerUser = (req, res) => {
+  res.json({ message: "Register User"})
+}
+
+// @desc Authenticate a user 
+// @route POST /api/users/login
+// @access Public
+const loginUser = (req, res) => {
+  res.json({ message: "Login User"})
+}
+
+// @desc Get user data 
+// @route GET /api/users/me
+// @access Public
+const getCurrentUser = (req, res) => {
+  res.json({ message: "Get current user data"})
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getCurrentUser,
+};
