c1e4d4b4d0780910a7afcac18573677af15d9df9
YouenEtrillard <contact@youen-etrillard.com>
2022-04-20 21:34:22 +0200

    add jwt to login and register responses

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,8 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user._id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400);
@@ -58,7 +59,8 @@ const loginUser = asyncHandler(async (req, res) => {
     res.json({
       _id: user._id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -72,6 +74,14 @@ const loginUser = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   res.json({ message: "Get current user data"})
 });
+
+
+// @desc Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
