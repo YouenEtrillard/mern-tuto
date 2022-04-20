3981998ca148d1402781a303b07ce3a05cfed629
YouenEtrillard <contact@youen-etrillard.com>
2022-04-20 20:29:57 +0200

    make registerUser method actually register a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,63 @@
-// @desc Register 
-// @route POST /api/users
-// @access Public
-const registerUser = (req, res) => {
-  res.json({ message: "Register User"})
-}
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
 
-// @desc Authenticate a user 
-// @route POST /api/users/login
-// @access Public
-const loginUser = (req, res) => {
+// @desc    Register 
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if(!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please fill in all fields')
+  }
+
+  // Check if user exists
+  const userExists = await User.findOne({ email });
+
+  if(userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if(user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+// @desc    Authenticate a user 
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "Login User"})
-}
+});
 
-// @desc Get user data 
-// @route GET /api/users/me
-// @access Public
-const getCurrentUser = (req, res) => {
+// @desc    Get user data 
+// @route   GET /api/users/me
+// @access  Public
+const getCurrentUser = asyncHandler(async (req, res) => {
   res.json({ message: "Get current user data"})
-}
+});
 
 module.exports = {
   registerUser,
