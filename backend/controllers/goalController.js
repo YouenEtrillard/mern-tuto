96f7b85746dec38d17e1c6e084b2735cf5531aed
YouenEtrillard <contact@youen-etrillard.com>
2022-04-22 17:07:23 +0200

    since protect middlware function was already getting and passing the user, we don't need to look for it again in editGoal, deleteGoal and getCurrentUser

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -40,16 +40,14 @@ const editGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
   // Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Erro('User not found');
   }
 
   // Make sure the logged in user matches the goal's user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorised");
   }
@@ -72,16 +70,14 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
   // Check for user
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Erro('User not found');
   }
 
   // Make sure the logged in user matches the goal's user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorised");
   }
