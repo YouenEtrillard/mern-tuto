0ed5b0e1f14de66484297e75cabf57a359a965e3
YouenEtrillard <contact@youen-etrillard.com>
2022-04-28 21:57:37 +0200

    1 more prettiered file

--- frontend/src/components/GoalItem/GoalItem.jsx
+++ frontend/src/components/GoalItem/GoalItem.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { deleteGoal, editGoal } from "../../features/goals/goalSlice";
-import { FaEdit, FaCheck, FaUndoAlt } from "react-icons/fa";
-import PropTypes from "prop-types"; 
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteGoal, editGoal } from '../../features/goals/goalSlice';
+import { FaEdit, FaCheck, FaUndoAlt } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
-
-function GoalItem({goal}) {
+function GoalItem({ goal }) {
   const dispatch = useDispatch();
   const [text, setText] = useState(goal.text);
   const [prevText, setPrevText] = useState(goal.text);
   const [isBeingEdited, setEdit] = useState(false);
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
-    dispatch(editGoal({id: goal._id, text}));
+    dispatch(editGoal({ id: goal._id, text }));
     setPrevText(text);
     setEdit(false);
   };
@@ -22,24 +21,32 @@ function GoalItem({goal}) {
   return (
     <li className="goal">
       <p>
-        {new Date(goal.createdAt).toLocaleString("fr-FR", {dateStyle: "long"})}
+        {new Date(goal.createdAt).toLocaleString('fr-FR', {
+          dateStyle: 'long',
+        })}
       </p>
       {isBeingEdited ? (
         <form onSubmit={onSubmit} action="">
-          <input
-            type="text"
-            name="text"
-            id="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
+          <label htmlFor="text">
+            Goal description
+            <input
+              type="text"
+              name="text"
+              id="text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </label>
           <button className="confirm-edit" type="submit">
             <FaCheck />
             <span className="visually-hidden">Confirm</span>
           </button>
           <button
-            className="undo-edit" 
-            onClick={() => {setText(prevText); setEdit(false);}}
+            className="undo-edit"
+            onClick={() => {
+              setText(prevText);
+              setEdit(false);
+            }}
           >
             <FaUndoAlt />
             <span className="visually-hidden">Undo and close edit</span>
@@ -48,14 +55,13 @@ function GoalItem({goal}) {
       ) : (
         <h2>{text}</h2>
       )}
-      <button
-        className="close"
-        onClick={() => dispatch(deleteGoal(goal._id))}
-      >X</button>
-      <button
-        className="edit"
-        onClick={() => setEdit(true)}
-      ><FaEdit /><span className="visually-hidden">edit goal</span></button>
+      <button className="close" onClick={() => dispatch(deleteGoal(goal._id))}>
+        X
+      </button>
+      <button className="edit" onClick={() => setEdit(true)}>
+        <FaEdit />
+        <span className="visually-hidden">edit goal</span>
+      </button>
     </li>
   );
 }
