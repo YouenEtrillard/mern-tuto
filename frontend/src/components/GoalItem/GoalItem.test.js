529cc14cb9700f1581b2287bba09374f3b295292
YouenEtrillard <contact@youen-etrillard.com>
2022-04-28 21:58:29 +0200

    add test when opening and closing the goal editing

--- frontend/src/components/GoalItem/GoalItem.test.js
+++ frontend/src/components/GoalItem/GoalItem.test.js
@@ -1,22 +1,59 @@
 /* eslint-disable no-undef */
-import { render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "../../app/store";
-import GoalItem from "./GoalItem";
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../../app/store';
+import GoalItem from './GoalItem';
 
 const goal = {
-  createdAt: "2022-04-26",
+  createdAt: '2022-04-26',
   _id: 123,
-  text: "Goal description"
+  text: 'Goal description',
 };
-  
-describe("GoalItem", () => {
-  it("should render goal date formatted to fr", () => {
-    render(
-      <Provider store={store}>
-        <GoalItem goal={goal} />
-      </Provider>
-    );
-    expect(screen.getByText("26 avril 2022")).toBeInTheDocument();
+
+describe('GoalItem', () => {
+  describe('on mounted', () => {
+    beforeEach(() => {
+      render(
+        <Provider store={store}>
+          <GoalItem goal={goal} />
+        </Provider>
+      );
+    });
+
+    it('should render date formatted to fr', () => {
+      expect(screen.getByText('26 avril 2022')).toBeInTheDocument();
+    });
+
+    it('should render description', () => {
+      expect(screen.getByText('Goal description')).toBeInTheDocument();
+    });
+
+    it('should render form when edit button is clicked', () => {
+      expect(screen.getByText('Goal description')).toBeTruthy();
+
+      fireEvent.click(screen.getByText('edit goal'));
+
+      expect(
+        screen.getByRole('textbox', { name: 'Goal description' })
+      ).toBeTruthy();
+      expect(screen.getByRole('button', { name: 'Confirm' })).toBeTruthy();
+      expect(
+        screen.getByRole('button', { name: 'Undo and close edit' })
+      ).toBeTruthy();
+    });
+
+    it('should hide form when cancel button is clicked', () => {
+      fireEvent.click(screen.getByText('edit goal'));
+
+      expect(
+        screen.getByRole('button', { name: 'Undo and close edit' })
+      ).toBeTruthy();
+
+      fireEvent.click(
+        screen.getByRole('button', { name: 'Undo and close edit' })
+      );
+
+      expect(screen.getByText('Goal description')).toBeTruthy();
+    });
   });
 });
