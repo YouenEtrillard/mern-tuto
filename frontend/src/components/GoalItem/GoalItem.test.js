00546505f4ef6171f7930be0707f92ccae4add0e
YouenEtrillard <contact@youen-etrillard.com>
2022-05-03 22:56:03 +0200

    adding more test for goalItem Not sure how much I need to mock for delete event. Might be easier to handle if the delete dispatch came from the list since the item is asking to delete itself

--- frontend/src/components/GoalItem/GoalItem.test.js
+++ frontend/src/components/GoalItem/GoalItem.test.js
@@ -4,56 +4,87 @@ import { Provider } from 'react-redux';
 import { store } from '../../app/store';
 import GoalItem from './GoalItem';
 
-const goal = {
-  createdAt: '2022-04-26',
-  _id: 123,
-  text: 'Goal description',
-};
+const goals = [
+  {
+    createdAt: '2022-04-26',
+    _id: 123,
+    text: 'Goal description',
+  },
+  {
+    createdAt: '2022-05-12',
+    _id: 456,
+    text: 'Second goal',
+  },
+];
 
 describe('GoalItem', () => {
-  describe('on mounted', () => {
-    beforeEach(() => {
-      render(
-        <Provider store={store}>
-          <GoalItem goal={goal} />
-        </Provider>
-      );
-    });
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <GoalItem goal={goals[0]} />
+      </Provider>
+    );
+  });
 
+  describe('on mounted', () => {
     it('should render date formatted to fr', () => {
       expect(screen.getByText('26 avril 2022')).toBeInTheDocument();
     });
 
     it('should render description', () => {
-      expect(screen.getByText('Goal description')).toBeInTheDocument();
+      expect(screen.getByText(goals[0].text)).toBeInTheDocument();
+    });
+  });
+
+  it('should render form when edit button is clicked', () => {
+    expect(screen.getByText(goals[0].text)).toBeTruthy();
+
+    fireEvent.click(screen.getByText('Edit goal'));
+
+    expect(screen.getByRole('textbox', { name: goals[0].text })).toBeTruthy();
+    expect(screen.getByRole('button', { name: 'Confirm' })).toBeTruthy();
+    expect(
+      screen.getByRole('button', { name: 'Undo and close edit' })
+    ).toBeTruthy();
+  });
+
+  describe('when submitting changes', () => {
+    beforeEach(() => {
+      fireEvent.click(screen.getByText('Edit goal'));
+      fireEvent.change(screen.getByRole('textbox', { name: goals[0].text }), {
+        target: { value: 'Changed goal description' },
+      });
+      fireEvent.click(screen.getByRole('button', { name: 'Confirm' }));
     });
 
-    it('should render form when edit button is clicked', () => {
-      expect(screen.getByText('Goal description')).toBeTruthy();
-
-      fireEvent.click(screen.getByText('edit goal'));
-
-      expect(
-        screen.getByRole('textbox', { name: 'Goal description' })
-      ).toBeTruthy();
-      expect(screen.getByRole('button', { name: 'Confirm' })).toBeTruthy();
-      expect(
-        screen.getByRole('button', { name: 'Undo and close edit' })
-      ).toBeTruthy();
+    it('should update goal description', () => {
+      expect(screen.getByText('Changed goal description')).toBeTruthy();
     });
+  });
 
-    it('should hide form when cancel button is clicked', () => {
-      fireEvent.click(screen.getByText('edit goal'));
-
-      expect(
-        screen.getByRole('button', { name: 'Undo and close edit' })
-      ).toBeTruthy();
-
+  describe('when cancel button is clicked', () => {
+    beforeEach(() => {
+      fireEvent.click(screen.getByText('Edit goal'));
+      fireEvent.change(screen.getByRole('textbox', { name: goals[0].text }), {
+        target: { value: 'Changed goal description' },
+      });
       fireEvent.click(
         screen.getByRole('button', { name: 'Undo and close edit' })
       );
-
-      expect(screen.getByText('Goal description')).toBeTruthy();
     });
+
+    it('should hide form', () => {
+      expect(screen.getByText(goals[0].text)).toBeTruthy();
+    });
+
+    it('should keep the initial goal description', () => {
+      expect(screen.getByText(goals[0].text)).toBeTruthy();
+    });
+  });
+
+  it('should delete goal when clicking on delete button', () => {
+    fireEvent.click(screen.getByText(/Delete goal/i));
+    console.log(goals);
+    expect(screen.getByText(goals[0].text)).toBeFalsy();
   });
 });
